c62e6e8aad7c698a4807103af68f6a4fd61ab93a
bobbidigi <bobbidigi34@gmail.com>
2020-03-08 19:52:43 -0400

    rendering smurf

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,20 +1,36 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import {fetchSmurfs} from '../actions/actions'
 import "./App.css";
+import Smurf from './Smurf'
 
 
-export default function App() {
+export default function App(props) {
 
   const dispatch = useDispatch();
   const state = useSelector(state => state);
 
   useEffect(()=>{
+    dispatch(fetchSmurfs())
+    console.log(state)
+  },[])
 
-  })
-  
   return (
     <div className="App">
-      
-    </div>
+      { state.isLoading ? (<div className="spinner"/>) : (
+
+        <div>
+          {state.smurfs.map(item => 
+            <Smurf smurf={item} key={item.id}/>  
+          )}
+        </div>)}
+
+
+        {/* form  */}
+
+
+        
+
+</div>
   )
 }
