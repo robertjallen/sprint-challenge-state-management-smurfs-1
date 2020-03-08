c648a53fee0d6b67aee3cad4822df67090aedc6d
bobbidigi <bobbidigi34@gmail.com>
2020-03-08 19:16:51 -0400

    setup initial state and action creator variables

---
+++ smurfs/src/actions/actions.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const FETCH_SMURF_START = "FETCH_SMURF_START";
+export const FETCH_SMURF_SUCCESS = "FETCH_SMURF_SUCCESS";
+export const FETCH_SMURF_ERROR = "FETCH_SMURF_ERROR";
+
+export const ADD_SMURF = "ADD_SMURF";
---
+++ smurfs/src/reducers/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+    // our "success" state
+    smurfs: [],
+    // want to make sure we account for all possible states,
+    // including the loading and error states
+    isLoading: false,
+    error: null,
+    location: '',
+    type: ''
+  };
